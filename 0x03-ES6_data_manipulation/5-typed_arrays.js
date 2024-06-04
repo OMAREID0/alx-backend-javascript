3fe7f9dbc55b1b0cb88833ab3bf273dcf2b19609
OMAREID0 <omareid221b@gmail.com>
2024-06-05 00:10:44 +0300

    done

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,8 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
-  const buffer = new ArrayBuffer(length);
-  const dv1 = new DataView(buffer);
-  dv1.setInt8(position, value);
-  return buffer;
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
 }
