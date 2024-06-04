f9230a3046bae060686517ea73e8709ada97e5a4
OMAREID0 <omareid221b@gmail.com>
2024-06-05 00:05:36 +0300

    test

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(students) {
   if (Array.isArray(students)) {
-    students.reduce((total, student) => total.id || total + student.id);
+    students.reduce((total, student) => total + student.id);
   }
   return 0;
 }
